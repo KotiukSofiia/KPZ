93a03fcf526d69feb9138cce55885b2de18ac6ad
Kotiuk Sofiia <ipz231_ksv@student.ztu.edu.ua>
2025-04-04 11:09:57 +0300

    Task5-6

---
+++ Lab 3/LightHTML/LightHTML/Models/LightNode.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LightHTML
+{
+    public abstract class LightNode
+    {
+        public abstract string GetOuterHtml(int indentLevel = 0);
+        public abstract string GetInnerHtml(int indentLevel = 0);
+    }
+}
