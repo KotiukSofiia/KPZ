869150a8b69e708adad66b9c901aceaa13cd9a3f
Kotiuk Sofiia <ipz231_ksv@student.ztu.edu.ua>
2025-04-15 15:23:35 +0300

    .

--- Lab 4/SupportHandler/SupportHandler/Program.cs
+++ Lab 4/SupportHandler/SupportHandler/Program.cs
@@ -6,18 +6,15 @@ namespace SupportSystem
     {
         static void Main(string[] args)
         {
-            // Створення обробників
             SupportHandler levelOne = new LevelOneHandler();
             SupportHandler levelTwo = new LevelTwoHandler();
             SupportHandler levelThree = new LevelThreeHandler();
             SupportHandler levelFour = new LevelFourHandler();
 
-            // Налаштовуємо ланцюжок обробників
             levelOne.SetNextHandler(levelTwo);
             levelTwo.SetNextHandler(levelThree);
             levelThree.SetNextHandler(levelFour);
 
-            // Запуск процесу запиту
             Console.WriteLine("Welcome to the Support System!");
             levelOne.HandleRequest();
 
