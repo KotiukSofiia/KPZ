7d441509d8fe09e1718e3c054fc9f1b6f51bec62
Kotiuk Sofiia <ipz231_ksv@student.ztu.edu.ua>
2025-05-12 12:11:40 +0300

    Add Command pattern support

---
+++ MKR1/LightHTML/LightHTML/Command/AddElementCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace LightHTML.Command
+{
+    public class AddElementCommand : ICommand
+    {
+        private LightElementNode parent;
+        private LightNode child;
+
+        public AddElementCommand(LightElementNode parent, LightNode child)
+        {
+            this.parent = parent;
+            this.child = child;
+        }
+
+        public void Execute()
+        {
+            parent.AddChild(child);
+        }
+
+        public void Undo()
+        {
+            parent.GetChildren().Remove(child);
+        }
+    }
+}
+
