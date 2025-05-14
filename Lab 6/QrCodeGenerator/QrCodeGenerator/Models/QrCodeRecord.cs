aff5fe956dfc15c55619ab44e5239f81afe7b15f
Kotiuk Sofiia <ipz231_ksv@student.ztu.edu.ua>
2025-05-14 16:52:25 +0300

    Add QrCodeRecord model for database storage

---
+++ Lab 6/QrCodeGenerator/QrCodeGenerator/Models/QrCodeRecord.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace QrCodeGenerator.Models
+{
+    public class QrCodeRecord
+    {
+        public int Id { get; set; }
+        public string InputText { get; set; }
+        public string ImagePath { get; set; }
+        public string Format { get; set; }
+        public DateTime GeneratedAt { get; set; }
+    }
+
+}
