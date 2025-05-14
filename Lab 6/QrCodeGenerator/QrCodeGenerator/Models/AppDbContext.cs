497e450af00dd86b8f9c01351e2587a4676f7bec
Kotiuk Sofiia <ipz231_ksv@student.ztu.edu.ua>
2025-05-14 19:08:31 +0300

    Add AppDbContext

---
+++ Lab 6/QrCodeGenerator/QrCodeGenerator/Models/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QrCodeGenerator.Models;
+
+namespace QrCodeGenerator.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<QrCodeRecord> QrCodes { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    }
+
+}
