29949db7a8f4fa345766567da4e7eb57381f4353
Kotiuk Sofiia <ipz231_ksv@student.ztu.edu.ua>
2025-05-17 00:17:32 +0300

    add model

---
+++ Lab 6/QrCodeGenerator/QrCodeGenerator/Models/ViewModels копія/HistoryViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace QrCodeGenerator.Models.ViewModels
+{
+    using QrCodeGenerator.Models;
+    using System.Collections.Generic;
+
+    public class HistoryViewModel
+    {
+        public List<QrCodeRecord> Records { get; set; } = new();
+        public List<string> AvailableTypes { get; set; } = new();
+        public string SelectedType { get; set; } = "";
+        public string SelectedSort { get; set; } = "date";
+    }
+}
