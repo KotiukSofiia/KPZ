9b4434516b5c42c47178588e871c54912660a7f3
vt231_kso <vt231_kso@student.ztu.edu.ua>
2025-05-22 20:59:14 +0300

    Refactor GenerateQrCodeCommand to use Mediator pattern

--- Lab 6/QrCodeGenerator/QrCodeGenerator/Commands/GenerateQrCodeCommand.cs
+++ Lab 6/QrCodeGenerator/QrCodeGenerator/Commands/GenerateQrCodeCommand.cs
@@ -1,66 +1,21 @@
-﻿using QrCodeGenerator.Models;
+using QrCodeGenerator.Models;
 using QrCodeGenerator.Services;
 using System.ComponentModel.DataAnnotations;
 
 namespace QrCodeGenerator.Commands
 {
-    public class GenerateQrCodeCommand : ICommand<(QrInputModel input, string format), CommandResult>
+  public class GenerateQrCodeCommand : ICommand<(QrInputModel input, string format), CommandResult>
+  {
+    private readonly IQrCodeMediator _mediator;
+
+    public GenerateQrCodeCommand(IQrCodeMediator mediator)
     {
-        private readonly IQRService _qrService;
-        private readonly IQrContentFormatterService _formatter;
-        private readonly IRepository _repository;
-
-        public GenerateQrCodeCommand(
-            IQRService qrService,
-            IQrContentFormatterService formatter,
-            IRepository repository)
-        {
-            _qrService = qrService;
-            _formatter = formatter;
-            _repository = repository;
-        }
-
-        public CommandResult Execute((QrInputModel input, string format) parameters)
-        {
-            var (input, format) = parameters;
-
-            var validationResults = new List<ValidationResult>();
-            var isValid = Validator.TryValidateObject(input, new ValidationContext(input), validationResults, true);
-
-            if (!isValid)
-            {
-                var errorMessages = validationResults.Select(vr => vr.ErrorMessage);
-                return CommandResult.Fail(string.Join("; ", errorMessages));
-            }
-
-            string content = _formatter.Format(input);
-
-            if (string.IsNullOrWhiteSpace(content))
-            {
-                return CommandResult.Fail("Content is empty");
-            }
-
-            string imagePath = _qrService.GenerateQrCode(
-                content,
-                format,
-                input.ForegroundColor,
-                input.BackgroundColor,
-                input.Size
-            );
-
-            var record = new QrCodeRecord
-            {
-                InputText = content,
-                ImagePath = imagePath,
-                Format = format,
-                Type = input.QrType,
-                GeneratedAt = DateTime.UtcNow
-            };
-
-            _repository.AddQrCodeRecord(record);
-            _repository.SaveChanges();
-
-            return CommandResult.Ok(new { QrPath = imagePath, InputText = content });
-        }
+      _mediator = mediator;
     }
+
+    public CommandResult Execute((QrInputModel input, string format) parameters)
+    {
+      return _mediator.GenerateAndStoreQrCode(parameters.input, parameters.format);
+    }
+  }
 }
